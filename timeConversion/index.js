3c5854dc8ef0c78f603027a6d63fd00c016b7e78
Fernanda <engcivil.fernandasouza@gmail.com>
2021-11-19 18:26:45 -0300

    desafio timeConversion - revisao

--- timeConversion/index.js
+++ timeConversion/index.js
@@ -2,8 +2,10 @@
 // Nota: 12:00:00AM em um relógio de 12 horas é 00:00:00 em um relógio de 24 horas.
 // Nota: 12:00:00PM em um relógio de 12 horas é 12h00 em um relógio de 24 horas.
 
-const time1 = "11:30:00AM";
-const time2 = "11:30:00PM";
+const time1 = "12:40:22AM";
+const time2 = "05:50:00AM";
+const time3 = "11:30:00PM";
+const time4 = "12:40:22PM";
 
 function timeConversion(s) {
   const time = s;
@@ -13,7 +15,8 @@ function timeConversion(s) {
   if (time.slice(8) === "AM") {
     res = time.slice(0, 8);
     if (time.slice(0, 2) === "12") {
-      res = "00:00:00";
+      let horaParaTroca = time.slice(0, 2);
+      res = time.slice(0, 8).replace(horaParaTroca, "00");
     }
   }
 
@@ -22,11 +25,12 @@ function timeConversion(s) {
     let hora = Number(horaParaTroca) + 12;
     res = time.slice(0, 8).replace(horaParaTroca, hora);
     if (time.slice(0, 2) === "12") {
-      res = "12:00:00";
+      let horaParaTroca = time.slice(0, 2);
+      res = time.slice(0, 8).replace(horaParaTroca, "12");
     }
   }
 
   return res;
 }
 
-timeConversion(time1);
+timeConversion(time3);
