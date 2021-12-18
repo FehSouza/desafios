d0497f2487d5c1f9379e148e4ec7c21352c87617
Fernanda <engcivil.fernandasouza@gmail.com>
2021-12-18 11:53:25 -0300

    exerciciosMDN

---
+++ exerciciosMDN.js
@@ -0,0 +1,41 @@
+// 1. EXERCÍCIOS FUNÇÕES:
+
+// 1.1. Exercício função:
+// const names = [
+//   "Chris",
+//   "Li Kang",
+//   "Anne",
+//   "Francesca",
+//   "Mustafa",
+//   "Tina",
+//   "Bert",
+//   "Jada",
+// ];
+
+// const chooseName = () => {
+//   const numberRandom = Math.floor(Math.random() * 8);
+//   console.log(names[numberRandom]);
+// };
+
+// chooseName();
+
+// 1.2. Exercício função:
+const names = [
+  "Chris",
+  "Li Kang",
+  "Anne",
+  "Francesca",
+  "Mustafa",
+  "Tina",
+  "Bert",
+  "Jada",
+];
+
+const getWordsCharacterLimit = (arr, limit) => {
+  const shortNames = arr.filter((elem) => {
+    if (elem.length < limit) return elem;
+  });
+  return shortNames;
+};
+
+console.log(getWordsCharacterLimit(names, 5));
---
+++ minimumDistances/index.js
@@ -0,0 +1,12 @@
+// A distância entre dois valores de um array é o número de índices entre eles. Dado "a", encontre a distância mínima
+// entre qualquer par de elementos iguais no array. Se esse valor não existir, retorne -1. Exemplo: a = [3, 2, 1, 2, 3]
+// Existem dois pares de valores correspondentes: 3 e 2. Os índices do 3 são i = 0 e j = 4, então a distância deles é
+// d[i, j] = |j - i| = 4. Os índices do 2 são i = 1 e j = 3, então a distância deles é d[i, j] = |j - i| = 2. A distância
+// mínima é 2.
+
+const minimumDistances = (a) => {
+  
+};
+
+console.log(minimumDistances([3, 2, 1, 2, 3]), 2);
+console.log(minimumDistances([7, 1, 3, 4, 1, 7]), 3);
