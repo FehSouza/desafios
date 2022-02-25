b1ce2324f15aa6d9452ac8b6e7a60d12607637ee
Fernanda <engcivil.fernandasouza@gmail.com>
2022-02-25 15:05:26 -0300

    exercicio map, filter e reduce

--- exercicios/MapFilterReduceII.js
+++ exercicios/MapFilterReduceII.js
@@ -13,24 +13,29 @@
 // });
 // console.log(temperatures);
 
+
+
 // EXERCÍCIO 2:
 // Remover elementos duplicados de um array:
 // esse exercício deve ser resolvido de 2 formas com filter e posteriormente com  reduce.
 // EXEMPLO: [ 1, 2, 3, 4, 5...]
 
 // const duplicated = [5, 7, 8, 9, 10, 5, 7, 6, 3, 2, 8, 6, 1, 10, 50, 20, 30, 0];
+
 // const duplicatedFormat = [...duplicated].sort((a, b) => a - b);
 
 // const withoutDuplicated = duplicatedFormat.filter((elem, index) => {
 //   if (elem !== duplicatedFormat[index + 1]) return true;
 // });
-// console.log(duplicatedFormat, withoutDuplicated);
+// console.log(withoutDuplicated);
 
 // const withoutDuplicated = duplicatedFormat.reduce((acc, elem, index) => {
 //   if (elem !== duplicatedFormat[index + 1]) return [...acc, elem];
 //   return acc;
 // }, []);
-// console.log(duplicatedFormat, withoutDuplicated);
+// console.log(withoutDuplicated);
+
+
 
 // EXERCÍCIO 3:
 // Realizar a soma de lançamentos de foguetes em um período de 1 ano:
@@ -47,16 +52,17 @@
 //   { country: "Japan", launches: 3 },
 // ];
 
-// const launches = rocketsLaunches.reduce(
-//   (acc, elem) => {
+// const launches = rocketsLaunches.reduce((acc, elem) => {
+//   if (acc.launches && acc.countries)
 //     return {
 //       launches: acc.launches + elem.launches,
 //       countries: acc.countries + 1,
 //     };
-//   },
-//   { launches: 0, countries: 0 }
-// );
+//   return { launches: elem.launches, countries: 1 };
+// }, {});
 // console.log(launches);
+
+
 
 // EXERCÍCIO 4:
 // Formatação de dados:
@@ -110,6 +116,8 @@
 // }, {});
 // console.log(categories);
 
+
+
 // EXERCÍCIO 5:
 // Filtro de filmes pela classificação
 // esse exercício deve retornar um objeto com os filmes classificados por classificação.
@@ -136,66 +144,124 @@
 // }, {});
 // console.log(classification);
 
+
+
 // EXERCÍCIO 6:
 // agrupamento de filmes por tipo de conteúdo
 // esse exercício deve retornar um objeto com os filmes agrupados por tipo de conteúdo.
-// EXEMPLO: { livroFilme: [], apenasLivro: [], apenasFilme: [], temLivro: [], temFilme: [] }
+// EXEMPLO: { bookFilm: [], onlyBook: [], onlyFilm: [], haveBook: [], haveFilm: [] }
 // deve ser resolvido com reduce.
-// nota: apenasLivro deve ser um array de livros que não tem conteúdo de filme
-// nota: apenasFilme deve ser um array de filmes que não tem conteúdo de livro
-// nota: temLivro deve ser um array que tem conteúdo de livro
-// nota: temFilme deve ser um array que tem conteúdo de filme
-// nota: livroFilme deve ser um array que tem conteúdo de livro e de filme
+// nota: onlyBook deve ser um array de livros que não tem conteúdo de filme
+// nota: onlyFilm deve ser um array de filmes que não tem conteúdo de livro
+// nota: haveBook deve ser um array que tem conteúdo de livro
+// nota: haveFilm deve ser um array que tem conteúdo de filme
+// nota: bookFilm deve ser um array que tem conteúdo de livro e de filme
 // nota: para facilitar iterar o objeto, utilize o método keys do objeto Object
 
-const harryPotterContent = {
-  "Harry Potter e a Pedra Filosofal": { livro: true, filme: true, ano: "2001" },
-  "Harry Potter e a Câmara Secreta": { livro: true, filme: true, ano: "2002" },
-  "Harry Potter e o Prisioneiro de Azkaban": {livro: true, filme: true,ano: "2004" },
-  "Harry Potter e o Cálice de Fogo": { livro: true, filme: true, ano: "2005" },
-  "Harry Potter e a Ordem da Fênix": { livro: true, filme: true, ano: "2007" },
-  "Os Contos de Beedle, o Bardo": { livro: true, filme: false, ano: "2008" },
-  "Harry Potter e o Enigma do Príncipe": {livro: true, filme: true,ano: "2009" },
-  "Harry Potter e as Relíquias da Morte: Parte 1": {livro: true, filme: true,ano: "2010" },
-  "Animais Fantásticos e Onde Habitam": {livro: false,filme: true,ano: "2016" },
-  "Animais Fantásticos: Os Crimes de Grindelwald": {livro: false,filme: true,ano: "2018" },
-  "Harry Potter e as Relíquias da Morte: Parte 2": {livro: true, filme: true,ano: "2011" },
-};
+// const harryPotterContent = {
+//   "Harry Potter e a Pedra Filosofal": { book: true, film: true, year: "2001" },
+//   "Harry Potter e a Câmara Secreta": { book: true, film: true, year: "2002" },
+//   "Harry Potter e o Prisioneiro de Azkaban": { book: true, film: true, year: "2004" },
+//   "Harry Potter e o Cálice de Fogo": { book: true, film: true, year: "2005" },
+//   "Harry Potter e a Ordem da Fênix": { book: true, film: true, year: "2007" },
+//   "Os Contos de Beedle, o Bardo": { book: true, film: false, year: "2008" },
+//   "Harry Potter e o Enigma do Príncipe": { book: true, film: true, year: "2009" },
+//   "Harry Potter e as Relíquias da Morte: Parte 1": { book: true, film: true, year: "2010" },
+//   "Animais Fantásticos e Onde Habitam": { book: false, film: true, year: "2016" },
+//   "Animais Fantásticos: Os Crimes de Grindelwald": { book: false, film: true, year: "2018" },
+//   "Harry Potter e as Relíquias da Morte: Parte 2": { book: true, film: true, year: "2011" },
+// };
 
-// { livroFilme: [], apenasLivro: [], apenasFilme: [], temLivro: [], temFilme: [] }
+// const listValues = Object.values(harryPotterContent);
+// const listFilms = Object.keys(harryPotterContent);
 
-const listValues = Object.values(harryPotterContent);
-const listFilms = Object.keys(harryPotterContent);
+// const classification1 = listValues.reduce((acc, elem, index) => {
+//   const element = { title: listFilms[index], ...elem };
 
-const classification = listValues.reduce((acc, elem, index) => {
-  if (elem.livro === true && elem.filme === true) {
-    if (acc.livroFilme)
-      return { ...acc, livroFilme: [...acc.livroFilme, `${listFilms[index]}: ${elem}`] };
-    return { ...acc, livroFilme: [listFilms[index]] };
-  }
-  if (elem.livro=== true && elem.filme === false) {
-    if (acc.apenasLivro) 
-      return { ...acc, apenasLivro: [...acc.apenasLivro, listFilms[index]] };
-    return { ...acc, apenasLivro: [listFilms[index]] };
-  }
+//   if (elem.book === true && elem.film === true) {
+//     if (acc.bookFilm) return { ...acc, bookFilm: [...acc.bookFilm, element] };
+//     return { ...acc, bookFilm: [element] };
+//   }
+//   if (elem.book && elem.film === false) {
+//     if (acc.onlyBook) return { ...acc, onlyBook: [...acc.onlyBook, element] };
+//     return { ...acc, onlyBook: [element] };
+//   }
+//   if (elem.book === false && elem.film) {
+//     if (acc.onlyFilm) return { ...acc, onlyFilm: [...acc.onlyFilm, element] };
+//     return { ...acc, onlyFilm: [element] };
+//   }
 
-  return acc;
-}, {});
-console.log(classification);
+//   return acc;
+// }, {});
+
+// const classification2 = listValues.reduce((acc, elem, index) => {
+//   const element = { title: listFilms[index], ...elem };
+
+//   if (elem.book) {
+//     if (acc.haveBook) return { ...acc, haveBook: [...acc.haveBook, element] };
+//     return { ...acc, haveBook: [element] };
+//   }
+
+//   return acc;
+// }, {});
+
+// const classification3 = listValues.reduce((acc, elem, index) => {
+//   const element = { title: listFilms[index], ...elem };
+
+//   if (elem.film) {
+//     if (acc.haveFilm) return { ...acc, haveFilm: [...acc.haveFilm, element] };
+//     return { ...acc, haveFilm: [element] };
+//   }
+
+//   return acc;
+// }, {});
+
+// const classification = {
+//   ...classification1,
+//   ...classification2,
+//   ...classification3,
+// };
+// console.log(classification);
+
+// const test = (obj) => {
+//   const test1 = obj.bookFilm.every((elem) =>
+//     elem.book && elem.film ? true : false
+//   );
+//   const test2 = obj.onlyBook.every((elem) =>
+//     elem.book && elem.film === false ? true : false
+//   );
+//   const test3 = obj.onlyFilm.every((elem) =>
+//     elem.book === false && elem.film ? true : false
+//   );
+//   const test4 = obj.haveBook.every((elem) => (elem.book ? true : false));
+//   const test5 = obj.haveFilm.every((elem) => (elem.film ? true : false));
+
+//   return {
+//     bookFilm: test1,
+//     onlyBook: test2,
+//     onlyFilm: test3,
+//     haveBook: test4,
+//     haveFilm: test5,
+//   };
+// };
+
+// console.log(test(classification));
+
+
 
 // EXERCÍCIO 7:
 // usar reduce para montar uma lista com os números pares e impares
 // { pares: [2,4,6...], impares: [1,3,5....] }
 
-// const arrayEx7 = [10, 50, 1, 2, 6, 8, 9, 11, 33, 41];
+const arrayEx7 = [10, 50, 1, 2, 6, 8, 9, 11, 33, 41];
 
-// const evenAndOdd = arrayEx7.reduce((acc, elem) => {
-//   if (elem % 2 === 0) {
-//     if (acc.even) return { ...acc, even: [...acc.even, elem] };
-//     return { ...acc, even: [elem] };
-//   } else {
-//     if (acc.odd) return { ...acc, odd: [...acc.odd, elem] };
-//     return { ...acc, odd: [elem] };
-//   }
-// }, {});
-// console.log(evenAndOdd);
+const evenAndOdd = arrayEx7.reduce((acc, elem) => {
+  if (elem % 2 === 0) {
+    if (acc.even) return { ...acc, even: [...acc.even, elem] };
+    return { ...acc, even: [elem] };
+  } else {
+    if (acc.odd) return { ...acc, odd: [...acc.odd, elem] };
+    return { ...acc, odd: [elem] };
+  }
+}, {});
+console.log(evenAndOdd);
